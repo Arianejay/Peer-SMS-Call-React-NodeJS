4fb4396c5cce540425f1410eef58ab30a619e5ee
Ariane Jay Tan <arianejaytantan@gmail.com>
2023-03-09 10:17:32 +0800

    add .gitignore on server

--- server/dist/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pino = require('pino');
-const logger = pino({
-    transport: {
-        target: 'pino-pretty'
-    },
-});
-exports.default = logger;
